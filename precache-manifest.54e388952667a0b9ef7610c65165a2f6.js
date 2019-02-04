08f9421efaef140711ed08d7ea83ac27e5b502a3
misaac3 <misaac3@wisc.edu>
2019-02-04 12:30:03 +0000

    Updates

---
+++ precache-manifest.54e388952667a0b9ef7610c65165a2f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "69c354017f0ef7241a37",
+    "url": "/nfl-draft/static/css/main.334722d3.chunk.css"
+  },
+  {
+    "revision": "69c354017f0ef7241a37",
+    "url": "/nfl-draft/static/js/main.69c35401.chunk.js"
+  },
+  {
+    "revision": "aae6a0ea265ea9995407",
+    "url": "/nfl-draft/static/css/1.53600771.chunk.css"
+  },
+  {
+    "revision": "aae6a0ea265ea9995407",
+    "url": "/nfl-draft/static/js/1.aae6a0ea.chunk.js"
+  },
+  {
+    "revision": "f211611807ccaf75746b",
+    "url": "/nfl-draft/static/js/runtime~main.f2116118.js"
+  },
+  {
+    "revision": "f45fc040d385b4de3be37c4f03ade9c5",
+    "url": "/nfl-draft/index.html"
+  }
+];
